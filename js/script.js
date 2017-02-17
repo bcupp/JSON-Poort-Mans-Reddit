db3f63fe607b076a28a942aa7e0aa9937f5f18d0
Brad Cupp <bradjcupp@gmail.com>
2017-02-17 15:43:15 -0500

    got data to display on page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 $(function() {
 
-  $.get('https://www.reddit.com/r/aww/.json').done(function(responseBody){
-    console.log('DONE', responseBody.data.children[0].data.title);
-  })
+  $.get('https://www.reddit.com/r/aww/top.json').done(function(responseBody){
+    responseBody.data.children.forEach(function(children){
+      var author = children.data.author;
+      var title = children.data.title;
+      var img = children.data.preview.images[0].source.url;
+      $('body').append($('<div>').html('<div>'+author+'<img src="'+img+'"/>'+title+'</div>'));
+    })
+  });
+
+
+
+
+
+
 
 
 });
