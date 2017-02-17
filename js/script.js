f24216a97eb9d1beb749546bb90b95d61cd0dfad
RedEis91 <imitationkrabs91@gmail.com>
2017-02-17 16:16:14 -0500

    style me plz

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function() {
       var author = children.data.author;
       var title = children.data.title;
       var img = children.data.preview.images[0].source.url;
-      $('body').append($('<div>').html('<div>'+author+'<img src="'+img+'"/>'+title+'</div>'));
+      $('body').append($('<div>').addClass('post').html('<div>'+'<h3>' +title +'</h3>'+author+'<div class="img_responsive">'+'<img src="'+ img+'"/>'+'</div>' +'</div>'));
     })
   });
 
