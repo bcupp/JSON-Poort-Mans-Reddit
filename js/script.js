d580622f235ed0cd0f3317bdbeecf3f96658c8c7
theadammason <adam.mason86@gmail.com>
2017-02-17 16:29:08 -0500

    formatting

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function() {
       var author = children.data.author;
       var title = children.data.title;
       var img = children.data.preview.images[0].source.url;
-      $('body').append($('<div>').addClass('post').html('<div>'+'<h3>' +title +'</h3>'+author+'<div class="img_responsive">'+'<img src="'+ img+'"/>'+'</div>' +'</div>'));
+      $('#content').append($('<div>').addClass('row post').html('<div class="col-md-12">'+'<h3>' +title +'</h3>'+author+'<img class="img-responsive" src="'+ img+'"/>' +'</div>'));
     })
   });
 
